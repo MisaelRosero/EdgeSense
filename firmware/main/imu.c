4ce66389a4f853fc9e2b954c42ea4f98f6f952fe
Misael Rosero <mr5364@princeton.edu>
2026-07-16 16:51:52 -0400

    Create initial IMU driver module structure

---
+++ firmware/main/imu.c
@@ -0,0 +1,4 @@
+#include "imu.h"
+#include "esp_log.h"
+
+static const char *TAG = "imu";
---
+++ firmware/main/imu.h
@@ -0,0 +1,8 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include "esp_err.h" 
+
+esp_err_t imu_init(void);
+
+#endif
